d21a868e0e64aa717015bb3d52dcefec3fe31960
Ben Plate <bplate9583@gmail.com>
2022-12-30 13:29:16 -0700

    change index route

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App () {
     <Router>
       <Navbar />
       <Routes>
-        <Route exact path='/index' element={<Home />} />
+        <Route exact path='/' element={<Home />} />
         <Route path='/about' element={<About/>} />
         <Route path='/contact' element={<Contact/>} />
         <Route path='/blog' element={<Blog/>} />

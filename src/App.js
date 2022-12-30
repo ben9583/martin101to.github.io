31ba3d90efe9229a0a62d874627edb7caff2d060
Martin101TO <87391349+Martin101TO@users.noreply.github.com>
2022-12-30 14:34:36 -0700

    added hashrouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './index.js'
 import Navbar from './components/navbar'
-import { BrowserRouter as Router, Routes, Route }
+import { HashRouter as Router, Routes, Route }
   from 'react-router-dom'
 import Home from './pages'
 import About from './pages/about'

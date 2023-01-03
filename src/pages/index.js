6315dac29682cb6f915c3d41e73dcc4326c2215f
Martin101TO <87391349+Martin101TO@users.noreply.github.com>
2023-01-03 15:37:23 -0700

    added stylesheet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import '../styles.css'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Future site of Martin101TO</h1>
-    </div>
+    <>
+      <link rel="stylesheet" href="styles.css"></link>
+      <div className='Home'>
+        <h1> Future site of Martin101TO</h1>
+      </div>
+      <div className='Portrait'>
+        <img src='../public/kevin.jpg' alt='Kevin Li' />
+      </div>
+    </>
   )
 }
 

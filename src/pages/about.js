d1f7d462d4894ce70b18076fe3f89a54031632bb
Martin101TO <87391349+Martin101TO@users.noreply.github.com>
2023-01-11 14:51:11 -0700

    began basic about me page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,16 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <>
+    <div id="about-page">
       <div className='PageTitle'>
         <h1>
           About Me: Kevin Li
         </h1>
       </div>
-      <div>
-
+      <div id='about-me'>
+        Hello, my name is Kevin Li and I am a software developer. I am currently a junior enrolled at the
+        University of Arizona W.A. Franke Honors College purusing a B.S. in Computer Science and a minor
+        in Mandarin language.
       </div>
-    </>
+    </div>
   )
 }
 

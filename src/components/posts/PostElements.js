99a7ce48c8b5469659894d5da6eebb03a7397322
Kevin Li <87391349+Martin101TO@users.noreply.github.com>
2023-01-12 22:11:41 -0700

    began blog

---
+++ src/components/posts/PostElements.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Post = styled.div`
+  border: 2px solid black;
+  height: 50px;
+  width: 50vw;
+  margin: 2vw;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`
